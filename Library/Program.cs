18815f668e216cd4ee305069685eca5b3b3e7a32
amykangweb <selene6023@gmail.com>
2016-02-07 13:52:42 -0800

    initial commit.

---
+++ Library/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    class Shelf
+    {
+        public static List<Book> arrayOfBooks = new List<Book>();
+
+        public static void Add(Book book)
+        {
+            arrayOfBooks.Add(book);
+        }
+    }
+    class Book
+    {
+        public string Title {
+            get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book();
+            book1.Title = "Wilbur's Wonderful Life";
+
+            Shelf.Add(book1);
+
+            foreach (var book in Shelf.arrayOfBooks)
+            {
+                Console.WriteLine(book.Title);
+            }
+
+            Console.Write("Please enter a book title: ");
+            string input = Console.ReadLine();
+
+            Book book2 = new Book();
+            book2.Title = input;
+
+            Shelf.Add(book2);
+
+            foreach(var book in Shelf.arrayOfBooks)
+            {
+                Console.WriteLine(book.Title);
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+<h1>Simple C# List Example</h1>
+
+<h2>Information</h2>
+
+By Amy Kang
+
+<h2>Description</h2>
+
+Simple example of creating objects and adding them to a list in C#.
